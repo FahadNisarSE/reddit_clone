ee072ee375c95c5376aa313325b162d9b24189e1
FahadNisarSE <fnisar142@gmail.com>
2023-07-02 08:14:24 +0500

    first_commit

---
+++ src/components/SignIn.tsx
@@ -0,0 +1,24 @@
+import { Icons } from "./icons";
+import Link from "next/link";
+
+const SignIn = () => {
+  return (
+    <div className="container mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]">
+      <div className="flex flex-col space-y-2 text-center">
+        <Icons.logo className="mx-auto h-5 w-6" />
+        <h1 className="text-2xl font-semibold tracking-tight">Welcome back</h1>
+        <p className="text-sm max-w-xs mx-auto">
+          By continuing, you are setting up a Reddit accoutnt and agree to our
+          User Agreement and Privacy Policy.
+        </p>
+
+        {/* Sign In Form */}
+
+        <p className="px-8 text-center text-sm text-zinc-700">New to Reddit?{" "} <Link href='/sign-up' className="hover:textg-zinc-800 text-sm underline underline-offset-4">Sign Up</Link></p>
+
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
